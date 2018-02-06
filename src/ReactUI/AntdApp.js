19b41c2cf3a694583a2c514f5333fb84b0cd3032
yuey9507 <YLi@esri.com>
2018-02-06 14:05:02 +0800

    Antd with rtlcss

--- src/ReactUI/AntdApp.js
+++ src/ReactUI/AntdApp.js
@@ -2,7 +2,7 @@
 * @Author: yuey9507
 * @Date:   2018-01-25 14:28:48
 * @Last Modified by:   yuey9507
-* @Last Modified time: 2018-01-29 18:32:46
+* @Last Modified time: 2018-02-05 17:57:53
 */
 import React from 'react';
 import {Button, DatePicker, Input, TimePicker, LocaleProvider} from 'antd';
@@ -10,9 +10,9 @@ import ar_EG from 'antd/lib/locale-provider/ar_EG';
 //import moment from 'moment';
 //import 'moment/locale/ar';
 //moment.locale('ar');
-
+//locale={ar_EG}
 const App = () => (
-  <LocaleProvider locale={ar_EG}>
+  <LocaleProvider >
   	<TimePicker />
   </LocaleProvider>
 );
--- src/ReactUI/main.js
+++ src/ReactUI/main.js
@@ -2,12 +2,12 @@
 * @Author: yuey9507
 * @Date:   2018-01-22 18:06:04
 * @Last Modified by:   yuey9507
-* @Last Modified time: 2018-01-30 17:16:01
+* @Last Modified time: 2018-02-05 17:29:32
 */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './OnsenApp.js';
+import App from './AntdApp.js';
 
 const render = (App) => {
   ReactDOM.render(
